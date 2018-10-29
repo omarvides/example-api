0c47ce88d80f157824be258b9b515bf0042ed8d0
omarvides <omarvides@gmail.com>
2018-10-28 21:19:03 -0600

    Adding structure

---
+++ tests/index.js
@@ -0,0 +1,19 @@
+const superTest = require('supertest');
+const app = require('../index');
+
+describe('Endpoints', () => {
+  describe('GET /', () => {
+    it('respond with 200', (done) => {
+      superTest(app)
+        .get('/')
+        .expect(200, done);
+    });
+  });
+  describe('POST /', () => {
+    it('should respond with 200 with an empty body', (done) => {
+      superTest(app)
+        .post('/')
+        .expect(200, done);
+    });
+  });
+});
